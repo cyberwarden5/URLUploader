a229f248987d2a6ec96c1d7f8155d37092333920
Bisnu Ray <79582318+bisnuray@users.noreply.github.com>
2024-11-02 02:27:15 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+# Pyrogram setup
+API_ID = "12345678"  # Replace this API ID with your actual API ID
+API_HASH = "XXXXXXXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "XXXXXXXXXXXX"  # Replace this BOT_TOKEN
+SESSION_STRING = "XXXXXXXXXXXXXXXXXXXXXX" # Replace this SESSION STRING with your actual SESSION_STRING
+
+MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024  # 2GB in bytes
