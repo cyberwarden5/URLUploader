6c4dcb5dda6547da8564ca1a32a8654f695bdc36
chestakor <139700193+chestakor@users.noreply.github.com>
2024-11-03 23:26:30 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "12345678"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "XXXXXXXXXXXX"  # Replace this BOT_TOKEN
-SESSION_STRING = "XXXXXXXXXXXXXXXXXXXXXX" # Replace this SESSION STRING with your actual SESSION_STRING
+API_ID = "28271744"  # Replace this API ID with your actual API ID
+API_HASH = "1df4d2b4dc77dc5fd65622f9d8f6814d"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8075052119:AAFEE8meQKG3Ncp8mj-nu7HbIC841rlg-Rg"  # Replace this BOT_TOKEN
+SESSION_STRING = "BQGvZIAAUHF3srxWv30V27BEa8DAUsiGKmuFSXavAylWb6XguSebGWVRgyAkxQRZIaGxSg6kdPogg_T_2QgIUpBhbRzgZ-Hfi2UpKX4el1HrchuvNyscbaWC2Tuxs3qH1w_1jJfME7Ze7epuUC3raE7mrwiwNDU9NQ7rSBro7NxFKdI4q04RD8xIOoeyWocvV1SlhF-HDeq1Og93o23mAPsMlLvIQW8ero1MnvJpnbzDR2YnEhSmpqKO9VidVVFlGTS7Q_Kqcck2Iatfsue_WXFUDPKcX75GazuiTuuuBOCJWt8Qft_nrpEnRwTV5K5fY2EXaByBglTKjrN2puFLZMQxXahrowAAAAGMkIjTAA" # Replace this SESSION STRING with your actual SESSION_STRING
 
 MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024  # 2GB in bytes
